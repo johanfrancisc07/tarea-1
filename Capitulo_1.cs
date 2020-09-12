0a3c63db197d722b13cf36cba13f63ef39d84bca
johanfrancisc07 <70924226+johanfrancisc07@users.noreply.github.com>
2020-09-12 12:41:31 -0400

    Add files via upload

---
+++ Capitulo_1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjerciciosLibro
+{
+    class Capitulo_1
+    {
+
+        public void Menu()
+        {
+            Console.WriteLine("------------------  MENU --------------------\n");
+            Console.WriteLine("----------------- Capitulo_1 ----------------");
+            Console.WriteLine("1. - Agregrar mi nombre");
+            Console.WriteLine("2. - Agregar mas mensajes a la aplicacion\n");
+            Console.WriteLine("----------------- Capitulo_2 ----------------");
+            Console.WriteLine("3. - Calcular Perimetro de un Poligono");
+            Console.WriteLine("4. - Convertir de Grados a Radianes");
+            Console.WriteLine("5. - Convertir de Dolares a Euro y Viciversa\n");
+            Console.WriteLine("----------------- Capitulo_3 -----------------");
+            Console.WriteLine("6. - Numero Par o Impar");
+            Console.WriteLine("7. - Numeracion de los dias de semana");
+            Console.WriteLine("8. - Calculo de Poligono\n");
+            Console.WriteLine("----------------- Capitulo_4 -----------------");
+            Console.WriteLine("9. -  Tabla de Multiplicacion");
+            Console.WriteLine("10. - Elevar numero a Cualquier Potencia");
+            Console.WriteLine("11. - Promedio de edad");
+        }
+
+
+        public void Ejercicio_1()
+        {
+            Console.WriteLine("johan");
+            Console.ReadLine();
+
+
+        }
+
+        public void Ejercicio_5()
+        {
+            Console.WriteLine("johan hernandez");
+            Console.WriteLine("809 952 0577");
+            Console.WriteLine("Salcedo");
+            Console.ReadLine();
+        }
+
+
+    }
+}
